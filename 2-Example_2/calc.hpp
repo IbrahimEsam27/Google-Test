c6f27e0748afd3b1884bfcf30062ef6af195bde5
Ibrahim Esam Ibrahim <ibrahimesam170@gmail.com>
2024-08-02 02:34:23 +0300

    calc.hpp

---
+++ 2-Example_2/calc.hpp
@@ -0,0 +1,15 @@
+#ifndef _CALC_H
+#define _CALC_H
+#include <iostream>
+
+class Student
+{
+public:
+    int score;
+    std::string name;
+    Student();
+    Student(int score, std::string name);
+    bool DisplayScore();
+};
+
+#endif // !_CALC_H#define _CALC_H
